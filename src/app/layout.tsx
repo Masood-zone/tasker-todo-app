27d5b97abc3125c239c9196123cde3cfd84f0343
masood <masoodacheampong@gmail.com>
2024-11-07 23:55:18 +0000

    added metadata images (favicon and description information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,16 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Todo App",
-  description: "A todo app built with Next.js",
+  title: "Tasker Todo",
+  description: "Enjoy your day with your own todo app",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
