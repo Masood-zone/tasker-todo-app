4c881fd4c8daf97c35046c828b4cf95b08da1754
masood <masoodacheampong@gmail.com>
2024-11-08 00:31:34 +0000

    added secret

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -3,5 +3,6 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   providers: [Google, GitHub],
 });
